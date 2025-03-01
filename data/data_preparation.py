9358356d5da234c93cb89c8c1be97c0e5417f17f
oganesova <lilush.stepanyan@mail.ru>
2025-03-01 15:13:19 +0400

    Data Preparation Scripts

---
+++ data/data_preparation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def dataset_cleaning(file_path):
+
+    df = pd.read_csv(file_path)
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    informal_df = df[['informal']].copy()
+    informal_df.rename(columns={'informal': 'text'}, inplace=True)
+    informal_df["label"] = 0
+
+    formal_df = df[['formal']].copy()
+    formal_df.rename(columns={'formal': 'text'}, inplace=True)
+    formal_df["label"] = 1
+
+    cleaned_df = pd.concat([informal_df, formal_df], ignore_index=True)
+
+    return cleaned_df
+
+def split_dataset(data_frame):
+
+    train, temp = train_test_split(data_frame, test_size=0.2, random_state=42)
+    val, test = train_test_split(temp, test_size=0.5, random_state=42)
+    train['text'] = train['text'].str.strip()
+    val['text'] = val['text'].str.strip()
+    test['text'] = test['text'].str.strip()
+    train.to_csv("../dataset/train_data.csv", index=False)
+    val.to_csv("../dataset/val_data.csv", index=False)
+    test.to_csv("../dataset/test_data.csv", index=False)
+
+if __name__ == "__main__":
+    dataset_path = "../dataset/formal_informal_dataset.csv"
+    clean = dataset_cleaning(dataset_path)
+    split_dataset(clean)
---
+++ data/data_preparation_spacy.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import spacy
+from datasets import Dataset, load_from_disk
+from sklearn.model_selection import train_test_split
+import os
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def preprocess_text(text):
+    doc = nlp(text)
+    cleaned_text = " ".join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
+    return cleaned_text
+
+def load_and_preprocess_data(csv_file):
+    df = pd.read_csv(csv_file)
+    df.columns = df.columns.str.strip()
+    df['cleaned_text'] = df.apply(lambda row: preprocess_text(row['formal']) if row.name % 2 == 0 else preprocess_text(row['informal']), axis=1)
+    df['label'] = df.apply(lambda row: 1 if row.name % 2 == 0 else 0, axis=1)
+    train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
+    val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
+    train_data = Dataset.from_pandas(train_df[['cleaned_text', 'label']])
+    val_data = Dataset.from_pandas(val_df[['cleaned_text', 'label']])
+    test_data = Dataset.from_pandas(test_df[['cleaned_text', 'label']])
+
+    return train_data, val_data, test_data
+
+def save_datasets(train_dataset, test_dataset, val_dataset):
+    train_dataset.save_to_disk('../dataset/train_spacy_dataset')
+    test_dataset.save_to_disk('../dataset/test_spacy_dataset')
+    val_dataset.save_to_disk('../dataset/val_spacy_dataset')
+
+if __name__ == "__main__":
+    dataset_path = "../dataset/formal_informal_dataset.csv"
+    train, val, test = load_and_preprocess_data(dataset_path)
+    save_datasets(train, val, test)
+
+
+
