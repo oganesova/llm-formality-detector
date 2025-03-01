ba90a077d5ea7877e652f6a8952c6d3ab91ac32f
oganesova <lilush.stepanyan@mail.ru>
2025-03-01 15:17:55 +0400

    Training model

---
+++ training/train_model_bert.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from datasets import Dataset
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+
+file_path_train = "../dataset/train_data.csv"
+file_path_val = "../dataset/val_data.csv"
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+def convert_frame_to_dataset_format(file_path):
+
+    data_df = pd.read_csv(file_path)
+    return Dataset.from_pandas(data_df)
+
+def tokenize(example):
+    model_inputs = tokenizer(
+        example["text"],
+        padding="max_length",
+        truncation=True,
+        max_length=128
+    )
+    model_inputs["labels"] = example["label"]
+
+    return model_inputs
+
+def get_tokenized_datasets():
+    train_dataset = convert_frame_to_dataset_format(file_path_train).map(tokenize, batched=True)
+    val_dataset = convert_frame_to_dataset_format(file_path_val).map(tokenize, batched=True)
+    return train_dataset, val_dataset
+
+def load_model():
+    train_dataset, val_dataset = get_tokenized_datasets()
+
+    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+
+    training_args = TrainingArguments(
+        output_dir="../models/bert_formality_classifier/",
+        evaluation_strategy="epoch",
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=3,
+        save_steps=500,
+        logging_dir="./logs",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+    )
+
+    trainer.train()
+    trainer.save_model("../models/bert_formality_classifier/")
+    print("Model training complete and saved!")
+
+if __name__ == "__main__":
+    print("Starting training...")
+    load_model()
---
+++ training/train_model_roberta.py
@@ -0,0 +1,57 @@
+from datasets import load_from_disk
+from transformers import Trainer, TrainingArguments, AutoTokenizer, \
+    AutoModelForSequenceClassification
+
+train_data_path = "../dataset/train_spacy_dataset"
+model_name = "roberta-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+def load_and_tokenize_data():
+    train_dataset = load_from_disk(train_data_path)
+
+    def tokenize_function(examples):
+        return tokenizer(examples["cleaned_text"], padding="max_length", truncation=True, max_length=128)
+
+    train_dataset = train_dataset.map(tokenize_function, batched=True)
+    train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
+
+    return train_dataset
+
+def initialize_model():
+    return AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+
+def setup_training_args():
+    return TrainingArguments(
+        output_dir="../models/roberta_formality_classifier/",
+        evaluation_strategy="no",
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        save_steps=500,
+        logging_dir="./logs"
+    )
+
+def train():
+    train_dataset = load_and_tokenize_data()
+    models = initialize_model()
+    training_args = setup_training_args()
+
+
+    trainer = Trainer(
+        model=models,
+        args=training_args,
+        train_dataset=train_dataset
+    )
+
+    trainer.train()
+
+    models.save_pretrained("../models/roberta_formality_classifier/")
+    tokenizer.save_pretrained("../models/roberta_formality_classifier/")
+
+    print("Model training complete and saved!")
+
+if __name__ == "__main__":
+    print("Starting training...")
+    train()
